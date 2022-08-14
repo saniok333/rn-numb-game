51adaf65cb1737df7e6a2543e6cec1181ec2dd97
saniok333 <alex.o.csc@gmail.com>
2022-08-14 11:28:07 +0300

    Setting Up Screen components

---
+++ screens/StartGameScreen.js
@@ -0,0 +1,12 @@
+import { View, TextInput } from 'react-native';
+
+const StartGameScreen = () => {
+  return (
+    <View>
+      <TextInput />
+      {/* Buttons */}
+    </View>
+  );
+};
+
+export default StartGameScreen;
